5b88c7e52f6048062176a8c516fd0299c98c6b10
nikiema2006 <nikiemalawal6@gmail.com>
2025-07-07 13:28:43 +0000

    nn

---
+++ app/analytics.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function analytics() {
+  return (
+    <View>
+      <Text>analytics</Text>
+    </View>
+  )
+}
